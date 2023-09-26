aadc2c95f3f7c19247345666761d4ccaeae3793a
Волков Кирилл Ильич <112119724+VolkovK04@users.noreply.github.com>
2023-09-26 20:07:59 +0300

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,6 @@
+_input = [Task(3, 23), Task(3, 10), Task(0, 30), Task(2, 50), Task(2, 13)]
+print(fast_solution(_input) == simple_solution(_input))
+_input = [Task(3, 74), Task(3, 93), Task(1, 88), Task(0, 94), Task(2, 39)]
+print(fast_solution(_input) == simple_solution(_input))
+_input = [Task(1, 37), Task(1, 64), Task(1, 54), Task(0, 39), Task(3, 49)]
+print(fast_solution(_input) == simple_solution(_input))
